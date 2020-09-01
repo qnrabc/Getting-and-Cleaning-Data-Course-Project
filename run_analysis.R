ab068eadd88638772ae213e48729c073eb337e1a
qnrabc <ajkunysz@gmail.com>
2020-09-01 18:15:07 +0200

    Added cleaning the dataset and saving it into a file.

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,6 @@
-##  Download the dataset  and unzip it
+library(reshape2)
+
+##  Download the dataset and unzip it
 
 dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
@@ -19,9 +21,36 @@ subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 x <- rbind(x_test, x_train)
 y <- rbind(y_test, y_train)
-subject <- rbind(subject_test, subject_train)
+s <- rbind(subject_test, subject_train)
 
 ## Load activity labels and features
 
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+activity_labels <- activity_labels[,2]
 features <- read.table("./UCI HAR Dataset/features.txt")
+
+## Take columns of x containing either std or mean patterns
+id_selected_columns <- grep("std|mean", features[,2])
+x <- x[,id_selected_columns]
+## Merge all the data
+data <- cbind(s, y, x)
+
+## Relabel feature columns
+colnames(data) <- c("subject", "activity", features[id_selected_columns,2])
+## Remove ()
+colnames(data) <- gsub("\\(\\)", "", colnames(data) )
+## Replace -mean with Mean
+colnames(data) <- gsub("-mean", "Mean", colnames(data) )
+## Replace -std with Std
+colnames(data) <- gsub("-std", "Std", colnames(data) )
+
+## Set descriptive activity names
+data$activity<-sapply(data$activity, function(x) activity_labels[x])
+
+## Create a second, independent tidy data set with the average of 
+## each variable for each activity and each subject.
+
+meltedData <- melt(data, id = c("subject", "activity") )
+tidy <- dcast(meltedData, subject + activity ~ variable, mean)
+
+write.table(tidy, file = "./tidy.txt", row.name=FALSE)
